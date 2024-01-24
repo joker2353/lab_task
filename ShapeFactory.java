92dd66a28f9bc6712a2f2572d41816c1d8eacdb0
SayakaMeem <sayakaalam419@gmail.com>
2024-01-24 08:59:08 +0600

    ShapeFactory is made singleton from factory pattern

---
+++ ShapeFactory.java
@@ -0,0 +1,25 @@
+public class ShapeFactory {
+    private static ShapeFactory instance=null;
+
+    private ShapeFactory() {
+    }
+
+    public static ShapeFactory getInstance() {
+        if (instance == null) {
+            instance = new ShapeFactory();
+        }
+        return instance;
+    }
+
+    public Shape getShape(String shapeType) {
+        if (shapeType == null) {
+            return null;
+        }
+        if (shapeType.equalsIgnoreCase("RECTANGLE")) {
+            return new Rectangle();
+        } else if (shapeType.equalsIgnoreCase("SQUARE")) {
+            return new Square();
+        }
+        return null;
+    }
+}
